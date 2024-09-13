f57e2a3af1609348da74252a1c8a85b918cafccb
ianmugedo26 <mugedoian@gmail.com>
2024-09-13 12:56:53 +0300

    area of rectangle

---
+++ area.c
@@ -0,0 +1,13 @@
+// Area of the rectangle
+#include<stdio.h>
+
+int main(){
+    int length,width,area;
+    printf("Enter the length");
+    scanf("%d",&length);
+    printf("Enter the width");
+    scanf("%d", &width);
+    area=length*width;
+    printf("The area of the length*width is %d",area);
+    
+    return 0;
